dbc7e2383aa2b0daef712a5afe4ecfac36ce16d7
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-02-08 21:24:52 +0400

    feature

---
+++ README.md
@@ -0,0 +1,1 @@
+[# Figma Link](https://www.figma.com/design/wUeBTUiH9RffXPhj0nnCqX/classwork-web3?node-id=0-1&t=Lg3EA9almSDEvinX-1)
---
+++ script.js
@@ -0,0 +1,49 @@
+      const products1 = [
+        {
+          title: "Gray T-Shirt",
+          price: "$14.99",
+          img: "./assets/GrayTShirt.png",
+        },
+        {
+          title: "Black Jeans",
+          price: "$34.99",
+          img: "./assets/BlackJeans.png",
+        },
+        {
+          title: "Green Jacket",
+          price: "$59.95",
+          img: "./assets/GreenJacket.png",
+        },
+        {
+          title: "White Sneakers",
+          price: "$49.95",
+          img: "./assets/WhiteSneakers.png",
+        },
+      ];
+
+      const products2 = [
+        {
+          title: "Blue Sweatshirt",
+          price: "$29.95 - $179.95",
+          img: "./assets/BlueSweatshirt.png",
+          colors: ["blue", "black"],
+        },
+        {
+          title: "Red Hoodie",
+          price: "$24.95 - $149.95",
+          img: "./assets/RedHoodie.png",
+          colors: ["red", "green"],
+        },
+        {
+          title: "Yellow Pullover",
+          price: "$19.95 - $129.95",
+          img: "./assets/YellowPullover.png",
+          colors: ["yellow", "orange"],
+        },
+        {
+          title: "Purple Jacket",
+          price: "$39.95 - $199.95",
+          img: "./assets/PurpleJacket.png",
+          colors: ["purple", "pink"],
+        },
+      ];
